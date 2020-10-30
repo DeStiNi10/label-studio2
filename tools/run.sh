d414d9af0b66f6fb2a8c0789634f77e74ca934d2
DeStiNi10 <atul.narayanan@gmail.com>
2020-10-30 10:16:04 +0530

    run.sh 2nd try

--- tools/run.sh
+++ tools/run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-label-studio start-multi-session --root-dir ./session_projects \
-             --host ${HOST:-0.0.0.0} --port ${PORT:-8080} --protocol ${PROTOCOL:-http://} \
-             --username ${USERNAME:-""} --password ${PASSWORD:-""}
+label-studio start-multi-session --root-dir ./session_projects --host ${HOST:-0.0.0.0} --port ${PORT:-8080} --protocol ${PROTOCOL:-http://}
